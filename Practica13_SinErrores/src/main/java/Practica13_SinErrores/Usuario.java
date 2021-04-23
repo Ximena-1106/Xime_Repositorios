d39a0ca5d702b3a747ce2ff0017838fb0c85b41b
Guadalupe Ximena Tamalatzi Osorio <xime1106to@gmail.com>
2021-04-23 15:01:53 -0500

    Practica12

---
+++ Practica13_SinErrores/src/main/java/Practica13_SinErrores/Usuario.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Practica13_SinErrores;
+
+/**
+ *
+ * @author nueva
+ */
+public class Usuario {
+
+}
